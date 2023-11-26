bf461ea4c9de4f70ce3a4fd55568d04f7b2ad34b
MBilalShaikh2003 <141023004+MBilalShaikh2003@users.noreply.github.com>
2023-11-26 06:40:57 -0800

    update

---
+++ Chapter-14-16-Arrays/Q1-Q7/app.js
@@ -0,0 +1,41 @@
+//Q1
+var studentnames=[];
+//Q2
+studentnames[0]="bilal";
+studentnames[1]="kamil";
+studentnames[2]="ali";
+//Q3
+var stringarray=[];
+stringarray[0]="a";
+stringarray[1]="b";
+stringarray[2]="c";
+//Q4
+var numarray=[];
+numarray[0]=1;
+numarray[1]=2;
+numarray[2]=3;
+//Q5
+var boolarray=[];
+boolarray[0]=true;
+boolarray[1]=false;
+//Q6
+var mixarray=[];
+mixarray[0]=false;
+mixarray[1]=1;
+mixarray[2]=true;
+mixarray[3]="a";
+//Q7
+var educationqualification_Array=[];
+educationqualification_Array[0]="SSC";
+educationqualification_Array[1]="HSC";
+educationqualification_Array[2]="BCS";
+educationqualification_Array[3]="BS";
+educationqualification_Array[4]="BCOM";
+educationqualification_Array[5]="MS";
+educationqualification_Array[6]="M.PHIL";
+educationqualification_Array[7]="Phd";
+var a="Qualifications"
+document.write(a + "<br/>");
+for(i=0;i<educationqualification_Array.length;i++){
+    document.write(i+1 +") "+educationqualification_Array[i]+"<br>");
+}
