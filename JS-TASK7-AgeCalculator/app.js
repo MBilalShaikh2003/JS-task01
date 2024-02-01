67d15bd01a4d95de675ae9d6698aec0946e8b0c4
MBilalShaikh2003 <141023004+MBilalShaikh2003@users.noreply.github.com>
2024-02-01 07:13:37 -0800

    change

--- JS-TASK7-AgeCalculator/app.js
+++ JS-TASK7-AgeCalculator/app.js
@@ -1,23 +1,37 @@
-
+function storeDate(){
+var dateInput = document.getElementById('inputDate');
+var inputValue = dateInput.value;
+var dob=new Date(inputValue).getTime();
+var currentdate=new Date().getTime();
+var diff=currentdate-dob;
+var year=Math.floor( diff/31536000000);
+console.log(year)
 
 function getyear(){
-    var dob=new Date("2 september,2003").getTime();
+    var dob=new Date(inputValue).getTime();
     var currentdate=new Date().getTime();
     var diff=currentdate-dob;
     var year=Math.floor( diff/31536000000);
+    
     return year;
 }
+document.getElementById("demo").innerHTML=getyear();
 function getmonth(){
-    var dob=new Date("2 september,2003").getTime();
+    var dob=new Date(inputValue).getTime();
     var currentdate=new Date().getTime();
     var diff=currentdate-dob;
     var month=Math.floor(diff/(1000*60*60*24)%12);
+    //console.log(month)
     return month;
 }
+document.getElementById("demo1").innerHTML=getmonth();
 function getday(){
-    var dob=new Date("2 september,2003").getTime();
+    var dob=new Date(inputValue).getTime();
     var currentdate=new Date().getTime();
     var diff=currentdate-dob;
     var day=Math.floor(diff/(1000*60*60*24)%365);
+    //console.log(day)
     return day;
+}
+document.getElementById("demo2").innerHTML=getday();
 }
